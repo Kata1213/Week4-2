f838cb3f2b0ea9347a8afdb436011f51e80210fe
Kata <2726456039@qq.com>
2018-04-13 13:05:36 +0800

    all test

--- src/test/java/com/thoughtworks/FizzBuzzGameTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzGameTest.java
@@ -15,5 +15,48 @@ public class FizzBuzzGameTest {
          assertEquals("Fizz", new FizzBuzzGame().getAnswer(6));
     }
 
+    @Test
+    public void should_return_Buzz_when_have_5() {
+       assertEquals("Buzz", new FizzBuzzGame().getAnswer(5));
+    }
+
+    @Test
+    public void should_return_Buzz_when_have_5_times() {
+        assertEquals("Buzz", new FizzBuzzGame().getAnswer(6));
+    }
+
+    @Test
+    public void should_return_Whizz_when_have_7() {
+        assertEquals("Whizz", new FizzBuzzGame().getAnswer(7));
+    }
+    @Test
+    public void should_return_Whizz_when_have_7_times() {
+        assertEquals("Whizz", new FizzBuzzGame().getAnswer(14));
+    }
+
+    @Test
+    public void should_return_FizzBuzz_when_have_15() {
+        assertEquals("FizzBuzz", new FizzBuzzGame().getAnswer(15));
+    }
+
+    @Test
+    public void should_return_FizzWhizz_when_have_21() {
+        assertEquals("FizzWhizz", new FizzBuzzGame().getAnswer(21));
+    }
+
+    @Test
+    public void should_return_Fizz_when_have_35() {
+        assertEquals("Fizz", new FizzBuzzGame().getAnswer(35));
+    }
+
+    @Test
+    public void should_return_BuzzWhizz_when_have_70() {
+       assertEquals("BuzzWhizz", new FizzBuzzGame().getAnswer(70));
+    }
+
+    @Test
+    public void should_return_FizzBuzzWhizz_when_have_105() {
+        assertEquals("FizzBuzzWhizz", new FizzBuzzGame().getAnswer(105));
+    }
 
 }
