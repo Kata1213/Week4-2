997197ea737a31385e40b4739de886406bb5cd1f
Kata <2726456039@qq.com>
2018-04-13 12:00:35 +0800

    modified:   src/main/java/com/thoughtworks/FizzBuzzGame.java 	modified:   src/test/java/com/thoughtworks/FizzBuzzGameTest.java

--- src/main/java/com/thoughtworks/FizzBuzzGame.java
+++ src/main/java/com/thoughtworks/FizzBuzzGame.java
@@ -6,14 +6,34 @@ import java.util.List;
 
 public class FizzBuzzGame {
     private List<String> results = new ArrayList<>();
+    private Integer fizz;
+    private Integer buzz;
+    private Integer whizz;
+
+    FizzBuzzGame() {
+        fizz = 3;
+        buzz = 5;
+        whizz = 7;
+    }
 
     public void start(int count) {
-
+        for (int i = 1; i <= count; i++) {
+            this.results.add(getAnswer(i));
+        }
     }
 
     public List<String> getResults() {
         return results;
     }
+
+    String getAnswer(int n) {
+        if (n % 10 == fizz) {
+            return ""+fizz;
+        }
+        return  null;
+    }
 }
+
+
 
 
--- src/test/java/com/thoughtworks/FizzBuzzGameTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzGameTest.java
@@ -1,10 +1,18 @@
 package com.thoughtworks;
 
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class FizzBuzzGameTest {
     @Test
     public void should_return_Fizz_when_have_3() {
-
+        FizzBuzzGame game = new FizzBuzzGame();
+        int num1 = 3;
+        int num2 = 13;
+        String fizz = "Fizz";
+        assertEquals(fizz, game.getAnswer(num1));
+        assertNotEquals(fizz, game.getAnswer(num2));
     }
+
 }
